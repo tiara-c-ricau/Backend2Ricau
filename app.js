b0bf90e0ecafd0d59580adf95df152bf4ccd261a
tiara <ricautiara145@gmail.com>
2026-01-22 12:04:08 -0300

    committ3

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ import express from "express";
 import handlebars from "express-handlebars";
 import productsRouter from "./routes/products.router.js";
 import viewsRouter from "./routes/views.router.js";
+import path from "path";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
+// 👉 STATIC (ACÁ VA)
+app.use(express.static(path.join(process.cwd(), "src", "public")));
 
 // Handlebars
 app.engine("handlebars", handlebars.engine());
-app.set("views", "./src/views");
+app.set("views", path.join(process.cwd(), "src", "views"));
 app.set("view engine", "handlebars");
 
 // Routers
@@ -20,3 +23,5 @@ app.use("/api/products", productsRouter);
 app.use("/", viewsRouter);
 
 export default app;
+
+
